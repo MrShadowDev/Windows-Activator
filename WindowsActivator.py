403e76b19012a4170e8c7c28447794c72a1a494e
ⲘrṨhส∂ow <71973368+MrShadowDev@users.noreply.github.com>
2023-02-16 18:35:01 +0100

    More Complex
    In this version, the code is organized into a class called WindowsActivator which contains the application logic. The __init__ method initializes the main window and calls the create_widgets method to create the GUI components. The create_widgets method creates the Label and Button widgets, and the activate_windows method activates Windows based on the option chosen by the user. Finally, the run method starts the main event loop.
    
    This version of the code is more complex because it uses object-oriented programming concepts, such as classes and methods, to organize the code and make it more modular. It also separates the GUI code from the application logic, making it easier to maintain and update the code in the future.

--- WindowsActivator.py
+++ WindowsActivator.py
@@ -1,33 +1,37 @@
-from cgitb import text
 import tkinter as tk
 import os
-from tkinter import font as tkFont
 
-window = tk.Tk()
-window.title('Windows 10/11 activator')
-font = tkFont.Font(family='Helvetica', size=24)
-window.configure(bg='#133C55')
+class WindowsActivator:
+    def __init__(self, master):
+        self.master = master
+        self.master.title('Windows 10/11 activator')
+        self.master.configure(bg='#133C55')
+        
+        self.font = ("Helvetica", 24)
+        
+        self.create_widgets()
 
-tk.Label(text="Windows Activator by MrShadowDev",font=tkFont.Font(family='Helvetica', size=24),bg='#133C55',fg='#EBEBEB').grid(column=1, row=0)
+    def create_widgets(self):
+        tk.Label(self.master, text="Windows Activator by MrShadowDev", font=self.font, bg='#133C55', fg='#EBEBEB').grid(column=1, row=0)
+        home_button = tk.Button(self.master, text="Home", font=("Helvetica", 16), bg='#59A5D8', fg='#EBEBEB', command=lambda: self.activate_windows('home'))
+        pro_button = tk.Button(self.master, text="Pro", font=("Helvetica", 16), bg='#59A5D8', fg='#EBEBEB', command=lambda: self.activate_windows('pro'))
 
+        home_button.grid(column=1, row=1)
+        pro_button.grid(column=1, row=2)
 
-def active(option):
-    #Windows home activation
-    if(option=='home'):
-        os.system("slmgr /ipk TX9XD-98N7V-6WMQ6-BX7FG-H8Q99")
-        os.system("slmgr /skms s8.uk.to")
-        os.system("slmgr /ato")
-    #Windows PRO activation
-    elif(option=='pro'):
-        os.system("slmgr /ipk W269N-WFGWX-YVC9B-4J6C9-T83GX")
-        os.system("slmgr /skms s8.uk.to")
-        os.system("slmgr /ato")
-        print('cos')
-   
+    def activate_windows(self, option):
+        if option == 'home':
+            os.system("slmgr /ipk TX9XD-98N7V-6WMQ6-BX7FG-H8Q99")
+            os.system("slmgr /skms s8.uk.to")
+            os.system("slmgr /ato")
+        elif option == 'pro':
+            os.system("slmgr /ipk W269N-WFGWX-YVC9B-4J6C9-T83GX")
+            os.system("slmgr /skms s8.uk.to")
+            os.system("slmgr /ato")
+    
+    def run(self):
+        self.master.mainloop()
 
-home = tk.Button(text="home" ,font=tkFont.Font(family='Helvetica', size=16),bg='#59A5D8',fg='#EBEBEB', command=lambda : active('home')).grid(column=1,row=1)
-pro = tk.Button(text="pro" ,font=tkFont.Font(family='Helvetica', size=16),bg='#59A5D8',fg='#EBEBEB', command=lambda : active('pro')).grid(column=1,row=2)
-
-
-
-window.mainloop()
+if __name__ == '__main__':
+    app = WindowsActivator(tk.Tk())
+    app.run()
